1b56eabde9f99e4bb49df999d41556c9f8988167
sktlim <suikiat00@gmail.com>
2023-01-16 11:27:25 +0800

    added react router dom

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {
-  BrowserRouter as Router,
+  BrowserRouter,
   Routes,
   Route,
 } from "react-router-dom";
+import Dashboard from "./Pages/Dashboard";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+      <Routes>
+        <Route path='/*' element={<Dashboard />} />
+      </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
