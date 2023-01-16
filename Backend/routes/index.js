01100833f12a41bb8e8bb06cc08cb5d87740f9f1
changdaozheng <50071666+changdaozheng@users.noreply.github.com>
2023-01-16 17:39:48 +0800

    changes for hosting

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -9,7 +9,7 @@ import sampleRouter from "./sample.js"
 
 const router = Router();
 
-router.use("/status", status);
+router.use("/", status);
 router.use("/forecast", forecastRouter);
 router.use("/campaign", campaignRouter);
 router.use("/inventory", inventoryRouter);
