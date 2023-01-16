92b44b73b998f68fbaf9ac2938f7d176315043e2
Trigon25 <53526750+Trigon25@users.noreply.github.com>
2023-01-16 22:56:21 +0800

    Update week refresh icon

--- frontend/src/Pages/Dashboard.js
+++ frontend/src/Pages/Dashboard.js
@@ -84,29 +84,6 @@ function Dashboard() {
             </div>
             <div class="flex flex-row">
                 <div class="flex basis-1/4">
-                    {/* <div id="Inventory" class="flex flex-col h-screen bg-slate-200 absolute top-0 left-0 items-center">
-                        <h2 class="text-lg bg-slate-100 text-center w-full py-1 mt-4 ">Inventory</h2>
-
-                        <Choice item={items} setMake={make => setMake(make)} />
-                        <span class="h-1 w-3/5 mt-1 rounded bg-slate-400" />
-                        {make === "" ? null :
-                            <>
-                                <h3 class="text-base ">{make}</h3>
-                                {arr}
-                            </>
-                        }
-                        {shortfall && make !== "" ?
-                            <button
-                                class="inline-block rounded-md px-4 py-2 mt-4 text-base text-gray-700 bg-red-400 hover:text-gray-800 hover:bg-rose-300 active:text-gray-900 active:bg-red-400 active:shadow-sm"
-                                onClick={() => setShowModal(true)}
-                            >
-                                Resupply
-                            </button>
-                            : null}
-                        <ResupplyModal onClose={() => setShowModal(false)} show={showModal} make={make} shortfallArr={shortfallArr}
-                            setStockChange={c => setStockChange(c)}
-                        />
-                    </div> */}
                     <div id="Inventory" class="absolute top-0 left-0 flex flex-col w-1/4 h-screen max-w-xs min-w-max bg-slate-200 items-center">
                         <h2 class="text-lg bg-slate-100 text-center w-full py-1 mt-4 ">Inventory</h2>
                         <Choice item={items} setMake={make=>setMake(make)}/>
--- frontend/src/components/Textbox.js
+++ frontend/src/components/Textbox.js
@@ -71,19 +71,13 @@ function TextboxTwo(props) {
             <span class="absolute inset-y-0 right-0 grid w-10 place-content-center">
                 <button
                     type="button"
-                    class="rounded-full bg-rose-600 p-0.5 text-white hover:bg-rose-700"
+                    class="rounded-full bg-rose-500 p-0.5 text-white hover:bg-rose-600"
                     onClick={()=>props.setChange(amount)}
                 >
                     <span class="sr-only">Submit</span>
-                    <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    class="h-4 w-4"
-                    >
-                    <path
-                        d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z"
-                    />
+                    <svg width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-4 h-4">
+                        <path d="M20.9844 10H17M20.9844 10V6M20.9844 10L17.6569 6.34315C14.5327 3.21895 9.46734 3.21895 6.34315 6.34315C3.21895 9.46734 3.21895 14.5327 6.34315 17.6569C9.46734 20.781 14.5327 20.781 17.6569 17.6569C18.4407 16.873 19.0279 15.9669 19.4184 15" 
+                        stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                     </svg>
                 </button>
             </span>
