61f49d2a2f7052a04d7967ed0542fcaa64da3b18
Trigon25 <53526750+Trigon25@users.noreply.github.com>
2023-01-16 18:35:38 +0800

    device per name handling

---
+++ frontend/src/Modals/ResupplyModal.js
@@ -0,0 +1,49 @@
+import React from "react";
+import fileTray from "../Assets/fileTray.png"
+import Textbox from "../Components/Textbox";
+
+const ResupplyModal = props => {
+    let render = [];
+    for (let i=0;i<props.shortfallArr.length;i++)
+    {
+        render.push(
+            <div class="w-2/3 py-2 my-2 h-16 flex justify-evenly items-center bg-slate-200 rounded-full">
+                <p class="text-center">{props.shortfallArr[i]}</p>
+                <Textbox setChange={c=>props.setStockChange(c)} name={props.shortfallArr[i]}/>
+            </div>
+        )
+    }
+
+    if (!props.show){
+        return null;
+    }
+    else {
+        return(
+            <div class="fixed left-0 top-0 right-0 bottom-0 bg-black/5" onClick={props.onClose}>
+            <div onClick={e=>e.stopPropagation()}
+            class="fixed left-1/4 w-1/2 top-1/4 h-1/2 block rounded-xl border bg-gray-100 border-gray-100 p-8 shadow-xl"
+            >
+                <span
+                    class="absolute right-4 top-4 rounded-full bg-slate-200 px-3 py-1.5 text-xs font-medium text-slate-600"
+                >
+                    {props.make}
+                </span>
+
+                <div class="flex-col relative mt-4 h-full w-full text-gray-800">
+                    <div class="rounded-full bg-blue-100 py-4 flex justify-center align-middle">
+                        <img src={fileTray} alt="File SVG" class="w-10 h-10"></img>
+                        <h3 class="text-xl ml-4 mt-2 font-bold text-gray-900">Resupply</h3>
+                    </div>
+                    <div class="relative w-full h-44 flex justify-center">
+                        {render}
+                    </div>
+                    <button onClick={props.onClose} class=" absolute bottom-4 rounded-full bg-red-100 px-3 py-1.5 text-xs font-medium text-red-600">
+                        Close
+                    </button>
+                </div>
+            </div>
+            </div>
+        )
+    }
+}
+export default ResupplyModal;
---
+++ frontend/src/components/Textbox.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+function Textbox(props) {
+    const [amount, setAmount] = useState(0);
+
+    const onChangeHandler = event => {
+       setAmount(event.target.value);
+    };
+
+    return(
+        <div class="relative">
+            <label for="number" class="sr-only"> Amount </label>
+            <input
+            type="number"
+            id="number"
+
+            value={amount}
+            onChange={onChangeHandler}
+            class="w-full text-center rounded-md border-gray-200 py-2.5 pr-10 shadow-sm sm:text-sm"
+            />
+        
+            <span class="absolute inset-y-0 right-0 grid w-10 place-content-center">
+                <button
+                    type="button"
+                    class="rounded-full bg-rose-600 p-0.5 text-white hover:bg-rose-700"
+                    onClick={()=>props.setChange([props.name,amount])}
+                >
+                    <span class="sr-only">Submit</span>
+                    <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    class="h-4 w-4"
+                    >
+                    <path
+                        d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z"
+                    />
+                    </svg>
+                </button>
+            </span>
+        </div>
+    )
+}
+
+export default Textbox;
